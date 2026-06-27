385fa534ae2eaca03c0c88f52b9ec666bc8ffb64
David Chessington <davidadavaz@outlook.com>
2026-06-27 17:44:58 -0600

    fix: changed route to default image for new users

--- models.py
+++ models.py
@@ -10,15 +10,16 @@ class User(db.Model):
     name = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
-    profile_photo= db.Column(db.String(255), nullable=True, default="templates/static/default.png")
+    profile_photo= db.Column(db.String(255), nullable=True, default="default.png")
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     deleted_at = db.Column(db.DateTime)
 
-    def __init__(self,name,email,password):
+    def __init__(self,name,email,password,profile_photo="default.png"):
         self.name=name
         self.email=email
         self.password=password
+        self.profile_photo=profile_photo
 
     def __repr__(self):
         return f"<User {self.name}>"
@@ -28,6 +29,7 @@ class User(db.Model):
             "id": self.id,
             "name": self.name,
             "email": self.email,
+            "profile_photo":self.profile_photo,
             "created_at": self.created_at.isoformat() if self.created_at else None,
             "updated_at": self.updated_at.isoformat() if self.updated_at else None,
             "deleted_at": self.deleted_at.isoformat() if self.deleted_at else None
